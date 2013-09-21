669dfc46e218ef23bb7f76740e7796ca29e1ac73
Nathan Harms <nathanharms@gmail.com>
2013-09-20 20:14:25 -0600

    Fixed up Node to use ints and longs where appropriate and added some methods necessary for the solver.

--- assignment1/Node.java
+++ assignment1/Node.java
@@ -1,29 +1,62 @@
 import java.util.Map;
 
 public class Node{
+    private static long[][] penalties;
+    private static long[][] tooNearPenalties;
+
     private int[] tasks = new int[8];
-    private int[][] penalties;
-    private int penalty;
-    private Node parent;
 
-    public Node(int[][] penalties){
+    public Node(long[][] penalties, long[][] tooNearPenalties){
         this.penalties = penalties;
+        this.tooNearPenalties = tooNearPenalties;
     }
 
-    public Node(int[][] penalties, Node parent){
-        this.penalties = penalties;
-        this.penalty = parent.getPenalty();
+    public Node(Node parent){
+        for(int i = 0; i < 8; i++){
+            tasks[i] = parent.getTask(i);
+        }
     }
 
-    public void setTask(char machine, int task) throws Exception{
-        int machineNum = getMachineNumber(machine);
-        if(machineNum < 0 || machineNum > 7) throw new Exception("FAIL");
-        tasks[getMachineNumber(machine)] = task;
-        penalty += penalties[machineNum][task];
+    public void setTask(int machine, int task) throws Exception{
+        if(machine < 0 || machine > 7) throw new Exception("invalid machine");
+        if(task < 0 || task > 7) throw new Exception("invalid task");
+        if(tasks[machine] != -1) throw new Exception("machine already assigned");
+        for(int i = 0; i < 8; i++){
+            if(tasks[i] == task) throw new Exception("task already assigned");
+        }
+
+        tasks[machine] = task;
+    }
+    
+    public int getTask(int machine){
+        return tasks[machine];
     }
 
-    public int getPenalty(){
+    public long getPenalty(){
+        long penalty = 0;
+        for(int i = 0; i < 8; i++){
+            if(tasks[i] != -1){
+                penalty += penalties[i][tasks[i]];
+                if(tasks[(i + 1) % 7] != -1){
+                    penalty += tooNearPenalties[i][(i + 1) % 7];
+                }
+            }
+        }
         return penalty;
+    }
+
+    public long getTaskCount(){
+        long count = 0;
+        for(int i = 0; i < 8; i++){
+            if(tasks[i] != -1) count++;
+        }
+        return count;
+    }
+
+    public int getFreeMachine(){
+        int result = 0;
+        while(tasks[result] != -1) result++;
+        return result;
     }
 
     public static int getMachineNumber(char machine){
