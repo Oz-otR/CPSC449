32b5b9d6fa61bd8e42e7a2db9b3bb85a7677a020
Nathan Harms <nathanharms@gmail.com>
2013-10-21 23:33:05 -0600

    Improved the automated tester.

--- test.py
+++ test.py
@@ -5,15 +5,19 @@ from subprocess import call
 
 for filename in os.listdir("testing"):
   if (filename[-4:] == ".txt"):
+    print "---------------------------------------------------------"
+    print "File " + filename + ":" #+ str(output1 == output2)
+    print "----running Java-------"
     call(["java","-cp","assignment1", "Main", "testing/" + filename, "output1.txt"])
     # Run haskell program here!
+    print "----running Haskell----"
     # call(["runhaskell","assignment2/main.hs","testing/" + filename, "output2.txt"])
     output1 = open("output1.txt", 'r').read()
     output2 = "blah blah blah"
-    #output2 = open("output2.txt", 'r').read()
-    print "File " + filename + ":" + str(output1 == output2)
-    print "     " + output1.strip()
-    print "     " + output2.strip()
+#    output2 = open("output2.txt", 'r').read()
+    print "----RESULT-------------"
+    print output1.strip()
+    print output2.strip()
 #                                          ,~~~~~~~~~~~~~~~~~~~~,
 #                                        ,'   You guys!! What    ',
 #                                        |    does that code      |
