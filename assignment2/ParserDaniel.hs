6be6bac2f92b26d7cd9bef5b946a813e6c1a0d0f
djarmstr <djarm2009@gmail.com>
2013-11-01 16:35:17 -0600

    Update: move stuff around

--- assignment2/ParserDaniel.hs
+++ assignment2/ParserDaniel.hs
@@ -13,18 +13,8 @@ splitSpace :: (Char a) => [a] -> [a]
 
 parser :: (String a) => String -> [a]
 -- output type: ([tooNearPen] (2D list of ints),[machinePen] (2D list in ints),[tooNear] (2D list of bool),[forbidden] (2D list of bool),[forced] (list of (machine-task pairs)),[optionalErrorMessage])--
---input type? ["(a,b)","(c,d)","(e,f)","\n","Header2"...}--
---check current x of x:xs--
---if null then go to next datatype--
---otherwise [x:[currentdatatype]] ++ parser xs--
-parser input = parseTooNear $ parseMachinePenalty $ parseForbiddenTooNear $ ParseForbiddenAssign $ parseForced input
-
-(a,b)
-fst snd (a,b) = a
-
-
 toValidNum :: Int -> Int
-toValidNumb a 
+toValidNum a 
 	|a == 'a' = 0
 	|a == 'b' = 1
 	|a == 'c' = 2
@@ -34,10 +24,7 @@ toValidNumb a
 	|a == 'g' = 6
 	|a == 'h' = 7
 	|otherwise	= -1
-							
-							
-							
-							
+
 {-							
 (file)
 .
@@ -46,17 +33,28 @@ toValidNumb a
 .
 .
 -}
+{-
 parse (file)
 parse case getHeader file of:
 								header1: (function1 file):returnVal ++ parse file --when nextLine == "", return everything so far--
 								header2: (function2 file):returnVal ++ parse file
 	otherwise	returnVal
-	
-ParseForbiddenTooNear :: ([Bool] a, Int b) => [String, b] -> [a, b]
+-}
+
+ParseForbiddenTooNear :: ([String a], [Bool] b, Int c) => [String, b, c] -> [a, b, c]
 {-	read next line
 	if line==(x,y)
 		assign True, a[x[y]] and parse to next line ParseForbiddenTooNear [a,b+1]
 	if line == ""
 		return [a,b+1]
 -}
-	
+
+ParseForbiddenTooNear [a,b,c]
+		| 
+
+
+--input type? ["(a,b)","(c,d)","(e,f)","\n","Header2"...}--
+--check current x of x:xs--
+--if null then go to next datatype--
+--otherwise [x:[currentdatatype]] ++ parser xs--
+parser input = parseTooNear $ parseMachinePenalty $ parseForbiddenTooNear $ ParseForbiddenAssign $ parseForced input
