40ac8d240ddb7aef1bf770c0552af37ec64d3b1d
djarmstr <djarm2009@gmail.com>
2013-11-01 18:13:48 -0600

    Changed functions: things will now take 4 inputs

--- assignment2/ParserDaniel.hs
+++ assignment2/ParserDaniel.hs
@@ -1,5 +1,5 @@
 import Data.Char
-
+{-
 parseFunction :: (Char a) => [a] -> [a]
 parseFunction input
 	|input == '\n':_= " " ++ parseFunction xs
@@ -8,12 +8,19 @@ parseFunction input
 	where 	a=[x:xs]
 
 splitSpace :: (Char a) => [a] -> [a]
+-}
 
-
-
-parser :: ([[Int]] a, [[Bool]] b, (Int, Int) c) => String -> (a, a, b, b, [c], String)
-	parser input = parse input
+parser :: ([[Int]] a, [[Bool]] b, (Int, Int) c) => [String] -> (a, a, b, b, [c], String)
+	--parser input = parseTooNear $ parseMachinePen $ parseForbiddenTooNear $ ParseForbidden $ parseForced input - not sure about this for now, might be too complicated--
+parser input = (a, b, c, d, e, f)
+where input
 -- output type: ([tooNearPen] (2D list of ints),[machinePen] (2D list in ints),[tooNear] (2D list of bool),[forbidden] (2D list of bool),[forced] (list of (machine,task pairs (example: 1,a)),[optionalErrorMessage])--
+	-- parseForced input: (input string, input position, forced assignments, error message)--
+	-- parseForbidden input: (input string, input position, forced assignments, forbidden assignments, error)--
+	-- parseForbiddenTooNear input: (string, position, forced assignments, forbidden assignments, forbidden tooNears, error)--
+	-- parseMachinePen input: (string, position, forced assignments, forbidden assign, forbidden tooNears, machine Pens, error)--
+	-- parseTooNear input: (""		""				""					""				""					""		   , tooNear Pens, error)--
+	-- parseTooNear output: function output type --
 taskToValidNum :: Int -> Int
 taskToValidNum a 
 	|a == 'a' = 0
@@ -42,31 +49,73 @@ parse case getHeader file of:
 	otherwise	returnVal
 -}
 
-parseForbiddenTooNear :: ([String] a, [Bool] b, Int c) => (String, b, c) -> 
+parseForbiddenTooNear :: ([String] a, [Bool] b) => (a, b, Int, String) -> (a, b, Int, String)
 {-	read next line
 	if line==(x,y)
 		assign True, a[x[y]] and parse to next line ParseForbiddenTooNear [a,b+1]
-	if line == ""
+	if line == "" or '\n':_
 		return [a,b+1]
 -}
 
-parseForbiddenTooNear (a,b,c)
-	|a == /n:_ = ""
-	|a == "" = ""
-	|otherwise = parseLineForbiddenTooNear (a,b,c)
+parseForbiddenTooNear (a,b,c,d)
+	|d \= "" = (a,b,c,d)
+	|a == /n:_ = (a, b, c+1,d)
+	|a == "" = (a, b, c+1,d)
+	|otherwise = parseLineForbiddenTooNear (a,b,c,d)
 
-parseLineForbiddenTooNear :: ([String a], [Bool] b, Int c) => (String, b, c) -> (a, b, c)
-parseLineForbiddenTooNear (a,b,c) = parseForbiddenTooNear (a, parseB, c)
+parseLineForbiddenTooNear :: ([String a], [Bool] b) => (a, b, Int, String) -> (a, b, Int, String)
+parseLineForbiddenTooNear (a,b,c,d) = parseForbiddenTooNear (a, parseB, c+1, d)
 	where parseB = replace True x (b !! y)
 		  x = (a !! c) !! 1
 		  y = (a !! c) !! 2
---input type? ["(a,b)","(c,d)","(e,f)","\n","Header2"...}--
+
+
+parseForbidden :: ([String] a, [Bool] b) => (a, b, Int, String) -> (a, b, Int, String)
+{-	read next line
+	if line==(x,y)
+		assign True, a[x[y]] and parse to next line ParseForbidden [a,b+1]
+	if line == "" or '\n':_
+		return [a,b+1]
+-}
+
+parseForbidden (a,b,c,d)
+	|d /= "" = (a,b,c,d)
+	|a == /n:_ = (a, b, c+1,d)
+	|a == "" = (a, b, c+1,d)
+	|otherwise = parseLineForbidden (a,b,c,d)
+
+parseLineForbidden :: ([String a], [Bool] b, Int c, String d) => (a, b, c, d) -> (a, b, c, d)
+parseLineForbidden (a,b,c,d) = parseForbidden (a, parseB, c+1, d)
+	where parseB = replace True x (b !! y)
+		  x = (a !! c) !! 1
+		  y = (a !! c) !! 2
+-- --
+parseTooNearPen:: ([String] a, [Bool] b) => (a, b, Int, String) -> (a, b, Int, String)
+{-	read next line
+	if line==(x,y)
+		assign True, a[x[y]] and parse to next line ParseForbiddenTooNear [a,b+1]
+	if line == "" or '\n':_
+		return [a,b+1]
+-}
+
+parseTooNearPen (a,b,c,d)
+	|d \= "" = (a,b,c,d)
+	|a == /n:_ = (a, b, c+1, d)
+	|a == "" = (a, b, c+1, d)
+	|otherwise = parseLineForbiddenTooNear (a,b,c,d)
+
+parseLineTooNearPen :: ([String a], [Bool] b) => (a, b, Int, String) -> (a, b, Int, String)
+parseLineTooNearPen (a,b,c,d) = parseTooNearPen (a, parseB, c+1, d)
+	where parseB = replace z x (b !! y)
+		  x = (a !! c) !! 1
+		  y = (a !! c) !! 2
+		  z = (a !! c) !! 3
+
+
+
 --check current x of x:xs--
 --if null then go to next datatype--
 --otherwise [x:[currentdatatype]] ++ parser xs--
-parser input = parseTooNear $ parseMachinePenalty $ parseForbiddenTooNear $ ParseForbiddenAssign $ parseForced input
-
-
 
 {- insert element 0 list = element:list
    insert element index (x:xs) = x:(insert element (index - 1) xs)
