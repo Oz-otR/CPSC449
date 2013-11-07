195f5b19b8caf0d49d2c828a9d06ec3b595b4499
djarmstr <djarm2009@gmail.com>
2013-11-07 10:57:42 -0700

    Fixed your tabs, Daniel

--- assignment2/ParserDaniel.hs
+++ assignment2/ParserDaniel.hs
@@ -65,27 +65,27 @@ parse :: [String] -> (Constraint, [(Int, Int)], String) -> (Constraint, [(Int, I
 parse strList (c, p, str) | trace ("Parse: " ++ (strList !! 0)) False = undefined
 
 parse ("Name:":xs) (a1, b1, "") =
-	parse (tail (tail xs)) (a1, b1, "")
+    parse (tail (tail xs)) (a1, b1, "")
 
 parse ("forced partial assignment:":xs) (a1, b1, "") =
-	parse (firstOfThree y) (a1, secondOfThree y, thirdOfThree y)
-	where y = parseForcedPartials (xs, [], [])
+    parse (firstOfThree y) (a1, secondOfThree y, thirdOfThree y)
+    where y = parseForcedPartials (xs, [], [])
 
 parse ("forbidden machine:":xs) (a1, b1, "") =
-	parse (firstOfThree y) (Constraint (getTooNearC a1) (secondOfThree y) (getTooNearP a1) (getMachineP a1), b1, thirdOfThree y)
-	where y = parseForbiddenMachine (xs, blank2dBool 8 8 False, [])
+    parse (firstOfThree y) (Constraint (getTooNearC a1) (secondOfThree y) (getTooNearP a1) (getMachineP a1), b1, thirdOfThree y)
+    where y = parseForbiddenMachine (xs, blank2dBool 8 8 False, [])
 
 parse ("too-near tasks:":xs) (a1, b1, "") =
-	parse (firstOfThree y) (Constraint (secondOfThree y) (getMachineC a1) (getTooNearP a1) (getMachineP a1), b1, thirdOfThree y)
-	where y = parseTooNearTasks (xs, blank2dBool 8 8 False, []) 
+    parse (firstOfThree y) (Constraint (secondOfThree y) (getMachineC a1) (getTooNearP a1) (getMachineP a1), b1, thirdOfThree y)
+    where y = parseTooNearTasks (xs, blank2dBool 8 8 False, []) 
 
 parse ("machine penalties:":xs) (a1, b1, "") =
-	parse (firstOfThree y) (Constraint (getTooNearC a1) (getMachineC a1) (getTooNearP a1) (secondOfThree y), b1, thirdOfThree y)
-	where y = parseMachinePenalties (xs, blank2dInt 8 8 0, [])
+    parse (firstOfThree y) (Constraint (getTooNearC a1) (getMachineC a1) (getTooNearP a1) (secondOfThree y), b1, thirdOfThree y)
+    where y = parseMachinePenalties (xs, blank2dInt 8 8 0, [])
 
 parse ("too-near penalities":xs) (a1, b1, "") =
-	parse (firstOfThree y) (Constraint (getTooNearC a1) (getMachineC a1) (secondOfThree y) (getMachineP a1), b1, thirdOfThree y)
-	where y = parseTooNearPenalties (xs, blank2dInt 8 8 0, [])
+    parse (firstOfThree y) (Constraint (getTooNearC a1) (getMachineC a1) (secondOfThree y) (getMachineP a1), b1, thirdOfThree y)
+    where y = parseTooNearPenalties (xs, blank2dInt 8 8 0, [])
 
 parse [] (a1, b1, "") = (a1, b1, "")
 --parse (x:xs) (a1, b1, "") = parse xs (a1, b1, "")
@@ -109,42 +109,42 @@ parse a (a1, b1, c1)
 	
 parseForcedPartials ::  ([String], [(Int, Int)], String) -> ([String], [(Int, Int)], String)
 parseForcedPartials(a, b, c)
-	|head a == []		= (a,b,c)
-	|otherwise			= parseForcedPartials(tail a, ((firstInteger a),(taskNumber(secondCharacter a))):b, c)
+    |head a == []		= (a,b,c)
+    |otherwise			= parseForcedPartials(tail a, ((firstInteger a),(taskNumber(secondCharacter a))):b, c)
 
 parseForbiddenMachine :: ([String], [[Bool]], String) -> ([String], [[Bool]], String)
 parseForbiddenMachine (a,b,c)
-	|c /= "" = (a,b,c)
-	|head a == "" = (a, b, c)
-	|otherwise = parseLineForbidden (a,b,c)
+    |c /= "" = (a,b,c)
+    |head a == "" = (a, b, c)
+    |otherwise = parseLineForbidden (a,b,c)
 
 parseLineForbidden :: ([String], [[Bool]], String) -> ([String], [[Bool]], String)
 parseLineForbidden (a,b,c) 
-	|c /= "" = (a,b,c)
-	|head a == "" = (a,b,c)
-	|(firstInteger a) `notElem` [0..7] = (a,b,"invalid machine/task")
-	|(secondCharacter a) `notElem` [0..7] = (a,b,"invalid machine/task")
-	|otherwise = parseForbiddenMachine (tail a, parseB b (firstInteger a) (taskNumber (secondCharacter a)), c)
-	where parseB r s t = replace (replace True t (r !! s)) s r
+    |c /= "" = (a,b,c)
+    |head a == "" = (a,b,c)
+    |(firstInteger a) `notElem` [0..7] = (a,b,"invalid machine/task")
+    |(secondCharacter a) `notElem` [0..7] = (a,b,"invalid machine/task")
+    |otherwise = parseForbiddenMachine (tail a, parseB b (firstInteger a) (taskNumber (secondCharacter a)), c)
+    where parseB r s t = replace (replace True t (r !! s)) s r
 	
 	{-fst $ read fst q :: (Int, Char) (1,A) (B,C)	(1,'A') ('B','C')
 	taskNumber $ snd $ read fst q :: (Int, Char)-}
 	
 parseTooNearTasks :: ([String], [[Bool]], String) -> ([String], [[Bool]], String)
 parseTooNearTasks (a,b,c)
-	|c /= "" = (a,b,c)
-	|head a == "\n" = (a,b,c)
-	|head a == "" = (a, b, c)
-	|otherwise = parseLineTooNearTasks (a,b,c)
+    |c /= "" = (a,b,c)
+    |head a == "\n" = (a,b,c)
+    |head a == "" = (a, b, c)
+    |otherwise = parseLineTooNearTasks (a,b,c)
 
 parseLineTooNearTasks :: ([String], [[Bool]], String) -> ([String], [[Bool]], String)
 parseLineTooNearTasks (a,b,c) 
-	|c /= "" = (a,b,c)
-	|head a == "" = (a,b,c)
-	|(firstInteger a) `notElem` [0..7] = (a,b,"invalid machine/task")
-	|(secondCharacter a) `notElem` [0..7] = (a,b,"invalid machine/task")
-	|otherwise = parseTooNearTasks (tail a, parseB b (taskNumber (firstCharacter a)) (taskNumber (secondCharacter a)), c)
-	where parseB r s t = replace (replace True t (r !! s)) s r			-- (1,a) is not an (Int,Char)!
+    |c /= "" = (a,b,c)
+    |head a == "" = (a,b,c)
+    |(firstInteger a) `notElem` [0..7] = (a,b,"invalid machine/task")
+    |(secondCharacter a) `notElem` [0..7] = (a,b,"invalid machine/task")
+    |otherwise = parseTooNearTasks (tail a, parseB b (taskNumber (firstCharacter a)) (taskNumber (secondCharacter a)), c)
+    where parseB r s t = replace (replace True t (r !! s)) s r			-- (1,a) is not an (Int,Char)!
 	
 	
 parseMachinePenalties :: ([String], [[Int]], String) -> ([String], [[Int]], String)
@@ -155,8 +155,8 @@ parseMachinePenalties (a,b,c) = parseMachineReturn(parseMachineHelper (a,b,c,1))
 	j j j j j j j j-}
 parseMachineHelper :: ([String], [[Int]], String, Int) -> ([String], [[Int]], String, Int)
 parseMachineHelper (a,b,c,d)
-	|d > 8 =	(a,b,c,d)
-	|otherwise = parseMachineHelper (tail a, replace (map read $ words (head a) :: [Int]) d b, c, d+1)
+    |d > 8 =	(a,b,c,d)
+    |otherwise = parseMachineHelper (tail a, replace (map read $ words (head a) :: [Int]) d b, c, d+1)
 
 parseMachineReturn :: ([String], [[Int]], String, Int) -> ([String], [[Int]], String)
 parseMachineReturn (a,b,c,d) = (a,b,c)
@@ -164,13 +164,13 @@ parseMachineReturn (a,b,c,d) = (a,b,c)
 	
 parseTooNearPenalties :: ([String], [[Int]], String) -> ([String], [[Int]], String)
 parseTooNearPenalties (a,b,c)
-	|c /= "" = (a,b,c)
-	|head a == "" = (a, b, c)
-	|otherwise = parseLineTooNearPenalties (a,b,c)
+    |c /= "" = (a,b,c)
+    |head a == "" = (a, b, c)
+    |otherwise = parseLineTooNearPenalties (a,b,c)
 
 parseLineTooNearPenalties :: ([String], [[Int]], String) -> ([String], [[Int]], String)
 parseLineTooNearPenalties (a,b,c) = parseTooNearPenalties (tail a, parseB, c)
-	where parseB = replace (replace (thirdInteger a) (taskNumber (firstCharacter a)) (b !! taskNumber (secondCharacter a))) (taskNumber (secondCharacter a)) b
+    where parseB = replace (replace (thirdInteger a) (taskNumber (firstCharacter a)) (b !! taskNumber (secondCharacter a))) (taskNumber (secondCharacter a)) b
 	
 --------------------------------------------------------------------------
 --String functions to get the input we want
