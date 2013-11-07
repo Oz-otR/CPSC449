2e5592f9bef047ba9163dc9cacad60085dd47575
Nathan Harms <nathan@debian.home>
2013-11-06 23:53:46 -0700

    Added checks for garbage lines in parser.

--- assignment2/Printer.hs
+++ assignment2/Printer.hs
@@ -10,30 +10,26 @@ import Utils
 
 printer :: (Solution,String)->IO()
 printer (solution, []) = do
-	args<-getArgs
-	let writeFile = args !! 1
-	handle<-openFile writeFile WriteMode
-	{-
-	let penalty = getPenalty solution
-	let penaltyChar = show penalty
-	let solutionk = map taskLetter (getAssignment solution)
-	let solutionM = unwords(appendSpace solutionk) -}
-	let prntStatement = (
-			"Solution " 
-			++ unwords(appendSpace (map taskLetter (getAssignment solution)))
-			++ "; Quality: " ++ show( getPenalty solution))
-	
-	
-	hPutStrLn handle (prntStatement)
+  args<-getArgs
+  let writeFile = args !! 1
+  handle<-openFile writeFile WriteMode
+  {-
+  let penalty = getPenalty solution
+  let penaltyChar = show penalty
+  let solutionk = map taskLetter (getAssignment solution)
+  let solutionM = unwords(appendSpace solutionk) -}
+  let prntStatement = ("Solution " ++ unwords(appendSpace (map taskLetter (getAssignment solution)))++ "; Quality: " ++ show( getPenalty solution))
+  hPutStrLn handle (prntStatement)
 
-	
+  
 printer (solution, error) = do
-	args<-getArgs
-	let writeFile = args !! 0
-	handle<-openFile writeFile WriteMode
-	hPrint handle error
-	
-	
+  args<-getArgs
+  let writeFile = args !! 0
+  handle<-openFile writeFile WriteMode
+  --hPrint handle error
+  hPrint handle ("Printer: " ++ error)
+  
+  
 
 
 appendSpace :: String->[String]
